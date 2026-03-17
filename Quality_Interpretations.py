a5fbd3bbcfc5152c7cea3ee384c9570e5d4f4e9f
Jorge Vilchez <jvilchezolmedo@gmail.com>
2026-03-17 16:13:22 -0500

    Update Quality_Interpretations.py

--- Quality_Interpretations.py
+++ Quality_Interpretations.py
@@ -1,5 +1,5 @@
 Structural validation Syntaxis
-Basado en Workflow para dinámica molecular en GROMACS , II. Extracción de estructuras tridimensionales desde el Protein Data Bank (PDB) (Mauricio Cavani 2026)
+Basado en Workflow para dinámica molecular en GROMACS , II. Extracción de estructuras tridimensionales desde el Protein Data Bank (PDB) (Maurizio Cavani 2026)
 
 Clascore es el numero de overlaps por 1000 atomos, utiliazndo radio de van efdr vals, si es mayor a 20 es problematica, si es menos a 5 excelente, entonces haymucho noise artificial, un MD se distorcionara al minimizar, significa que tu imagen experimentalmente, cuando nonbonded atoms estan nmas scerca de loq ue deberian a pesar de su vandervalds.Osea no esta refinado más probablemente, side chain, rotamero incorrecto o qu tu tecnica esta interfiriendo en lo que uqieres hacer.
 Ramachandran Plot Determinada por rotacion, entonces favorable permitida y outliers. Favorable para el 98% es excelente, tambien muestran outliers. RMN es bueno para mostrar mayor distribucion, osea casos más permitidos , outliers más pemitidos. Es para flexibilidad de ensamblaje. RMN muestra mayor flexibilidad, menores constrant angulaes, RMn permite mas regiones permitidas. 
